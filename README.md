8f1428bafc0f9230e10713742c162a5488f0b37f
Ramiro Vignolo <ramirovignolo@gmail.com>
2021-02-23 20:22:37 -0300

    + more scripting

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Tutorials
 
 [![Build Status](https://github.com/rvignolo/Tutorials.jl/workflows/CI/badge.svg)](https://github.com/rvignolo/Tutorials.jl/actions)
+
+
+# SciQuant tutorials
+
+| **Documentation** |
+|:------------ |
+| [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://SciQuant.github.io/Tutorials/stable/) [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://SciQuant.github.io/Tutorials/dev/) |
+|**Build Status** |
+| [![Build Status](https://github.com/SciQuant/Tutorials/workflows/CI/badge.svg)](https://github.com/SciQuant/Tutorials/actions) |
+
+This repo contains a set of tutorials to learn how to use SciQuant's packages for quantitative finance in Julia.
+
+The tutorials are available in two formats:
+
+- As jupyter notebooks, allowing an interactive learning experience.
+
+- As HTML pages, allowing a rapid access into the material without the need of any setup.
+
+Checkout one of the following:
+
+- [**STABLE**](https://SciQuant.github.io/Tutorials/stable) &mdash; **Tutorials for the most recently tagged version of SciQuant's packages.**
+- [**DEVEL**](https://SciQuant.github.io/Tutorials/dev) &mdash; *Tutorials for the in-development version of SciQuant's packages.*
+
+## Maintainers and Contributors
+
+These packages are being developed by [SciQuant](https://github.com/SciQuant), an organization dedicated to creating high-quality scientific software for the financial industry.
--- src/simple_dynamics.jl
+++ src/simple_dynamics.jl
@@ -1,14 +1,71 @@
-# # Rational numbers
+# In this tutorial, you will learn:
 #
-# In julia rational numbers can be constructed with the `//` operator.
-# Lets define two rational numbers, `x` and `y`:
-
-## Define variable x and y
-x = 1//3
-y = 2//5
-
-# When adding `x` and `y` together we obtain a new rational number:
-
-z = x + y
+#    -  How to declare dynamics with arbitrary coefficients
+#    -  How to declare dynamics with known coefficients
+#    -  How to declare dynamical systems
+#
+#
+# # Introduction
+#
+# In **UniversalDynamics** a dynamics represents continuous time, ``D``-dimensional Ito
+# Systems of Stochastic Differential Equations (SDEs) in a time span ``\mathbb{I} =
+# \left[t_0, T \right]``:
+#
+# ```math
+# d\vec{u}(t) = f(t, \vec{u}(t)) \cdot dt + g(t, \vec{u}(t)) \cdot d\vec{W}(t), \quad \vec{u}(t_0) = \vec{u}_0,\\
+# ```
+#
+# with drift coefficient ``f \colon \mathbb{I} \times \mathbb{R}^D \rightarrow
+# \mathbb{R}^D``, diffusion coefficient ``g \colon \mathbb{I} \times \mathbb{R}^D
+# \rightarrow \mathbb{R}^{D \times M}``, ``M``-dimensional driving Wiener correlated or
+# uncorrelated process ``\vec{W}(t)`` and initial condition ``\vec{u}_0``.
+#
+# The library represents dynamics by means of two main types, `SystemDynamics` and
+# `ModelDynamics`. The first case refers to SDEs with arbitrary coefficients while the
+# second case is a supertype for dynamics with known coefficients that are already
+# implemented in the library.
+#
+# Finally, as discussed in the [documentation](https://sciquant.github.io/UniversalDynamics.jl/dev/ad/dynamics.html#Introduction),
+# the general expression of a dynamics can be reduced depending on the noise type.
+#
+## Setup
+#
+# The first step is to load the library:
 
 using UniversalDynamics
+
+## System Dynamics
+#
+# A System of Stochastic Differential Equations is declared by its initial condition,
+# initial time, noise type and noise correlations, for example:
+
+x = SystemDynamics(rand(1); noise=ScalarNoise())
+y = SystemDynamics(rand(2); noise=DiagonalNoise(2), ρ=[1 0.3; 0.3 1])
+z = SystemDynamics(rand(3); noise=NonDiagonalNoise(2), ρ=[1 0.2; 0.2 1])
+
+## Model Dynamics
+#
+# A Model Dynamics refers to Stochastic Differential Equations that are known and fairly
+# common in finance, such that is worth having their coefficients implemented in the
+# library. In this case,  we need to provide the additional known parameters to the
+# dynamics. For example, a One Factor Short Rate Model of Affine type that yields to the well
+# known Vasicek model, can be given as:
+r0 = rand(1)
+
+ϰ = 0.4363
+ϑ = 0.0613
+σ = 0.1491
+
+κ(t) = ϰ
+θ(t) = ϑ
+Σ(t) = σ
+
+r = OneFactorAffineModelDynamics(r0, κ, θ, Σ, one, zero)
+
+
+
+# group dynamics in a container
+dynamics = [:x => x, :y => y, :z => z]
+
+# compute dynamical system
+ds = DynamicalSystem(dynamics)
